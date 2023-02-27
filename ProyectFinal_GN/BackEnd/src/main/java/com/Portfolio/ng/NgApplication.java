30a8394fc3e686e950de100ade3e56c3d6cd7b1e
Nahuel Gomez <nagomez61@gmail.com>
2023-02-27 16:38:46 -0300

    Creamos la carpeta BackEnd

---
+++ ProyectFinal_GN/BackEnd/src/main/java/com/Portfolio/ng/NgApplication.java
@@ -0,0 +1,13 @@
+package com.Portfolio.ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgApplication.class, args);
+	}
+
+}
---
+++ ProyectFinal_GN/BackEnd/src/test/java/com/Portfolio/ng/NgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
